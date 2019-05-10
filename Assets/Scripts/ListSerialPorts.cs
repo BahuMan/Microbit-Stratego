ebf55bb90208fd171f4518fc28622f41b4d0cced
Bart Huylebroeck <bart.huylebroeck@pandora.be>
2019-05-10 22:57:50 +0200

    first commit

---
+++ Assets/Scripts/ListSerialPorts.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+using UnityEngine;
+
+public class ListSerialPorts : MonoBehaviour
+{
+
+    [SerializeField]
+    private SinglePortScript portPrefab;
+
+    private Dictionary<string, SinglePortScript> PortnameToGUIMap = new Dictionary<string, SinglePortScript>();
+
+    void Start()
+    {
+        StartCoroutine(UpdateSerialPorts());
+    }
+
+    private IEnumerator UpdateSerialPorts()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+
+            Dictionary<string, SinglePortScript> newPortsMap = new Dictionary<string, SinglePortScript>();
+            foreach (string portname in SerialPort.GetPortNames())
+            {
+                if (PortnameToGUIMap.ContainsKey(portname))
+                {
+                    //no need to create GUI element; it was created in a previous iteration
+                    newPortsMap[portname] = PortnameToGUIMap[portname];
+                    PortnameToGUIMap[portname].CheckStatus();
+                }
+                else
+                {
+                    SinglePortScript newPort = Instantiate<SinglePortScript>(portPrefab, this.transform);
+                    newPort.gameObject.SetActive(true);
+                    newPort.Name = portname;
+                    newPortsMap[portname] = newPort;
+                    newPort.CheckStatus();
+                }
+            }
+
+            //remove any GUI elements for ports that were no longer found:
+            foreach (string oldport in new List<string>(PortnameToGUIMap.Keys))
+            {
+                if (!newPortsMap.ContainsKey(oldport))
+                {
+                    GameObject GUI = PortnameToGUIMap[oldport].gameObject;
+                    Destroy(GUI);
+                }
+            }
+
+            PortnameToGUIMap = newPortsMap;
+        }
+    }
+
+}
---
+++ Assets/Scripts/SinglePortScript.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.IO.Ports;
+using System.IO;
+
+public class SinglePortScript : MonoBehaviour
+{
+
+    [SerializeField]
+    private Text portName;
+
+    [SerializeField]
+    private Text portStatus;
+
+    [SerializeField]
+    private Button portButton;
+
+    private SerialPort port;
+
+    public string Name
+    {
+        set {
+            portName.text = value;
+            if (port != null) port.Close();
+            port = new SerialPort(value, 115200, Parity.None, 8, StopBits.One);
+        }
+
+        get {
+            return portName.text;
+        }
+    }
+    public void CheckStatus()
+    {
+        try
+        {
+            if (!port.IsOpen) port.Open();
+            port.WriteLine("check");
+            if (port.BytesToRead > 0)
+            {
+                portStatus.text = port.ReadExisting().Trim();
+            }
+        }
+        catch (System.UnauthorizedAccessException uaae)
+        {
+            portStatus.text = "Unauthorized";
+        }
+        catch (System.ArgumentOutOfRangeException aoore)
+        {
+            portStatus.text = "Check params";
+        }
+        catch (IOException ioe)
+        {
+            portStatus.text = "IO error";
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (port != null && port.IsOpen) port.Close();
+    }
+}
